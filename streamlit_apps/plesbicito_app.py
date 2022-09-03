14294d0e6230c254b673ca1cf3ea83848b223ff4
ampacheco <ampacheco@users.noreply.github.com>
2022-09-03 01:59:48 -0400

    🎯 plesbicito app installed

---
+++ streamlit_apps/plesbicito_app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import datetime
+
+#
+# convert to function later
+#
+st.title('Plesbicito de Salida. Santiago de Chile')
+data_url='https://raw.githubusercontent.com/ampacheco/data-science-workout/main/data/data_2022.csv'
+df = pd.read_csv(data_url)
+
+# df["Date"]=pd.to_datetime(df["Date"])
+
+# to avoid time conversion
+df["Date"]=pd.to_datetime(df["Date"]).dt.date
+
+start_date=datetime.date.fromisoformat('2022-01-28')
+end_date=datetime.date.fromisoformat('2022-09-02')
+
+s_date=st.date_input('Fecha Inicial', start_date)
+e_date=st.date_input('Fecha Final', end_date)
+
+filtered_df=df[(df["Date"]>=s_date) & (df["Date"]<=e_date)]
+
+st.write(filtered_df)
+st.line_chart(filtered_df, x="Date", y=["Apruebo", "Rechazo", "NSNR"])
